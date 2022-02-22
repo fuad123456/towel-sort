dda03e5365bf1077c525228423fb66b64037efb7
Fuad Крым <fuadkrym@gmail.com>
2022-02-22 12:08:59 +0300

    feat: add package json and solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	let ar=[]
+	if(arguments.length===0) return []
+	if(matrix.length===0) return []
+	else{
+		for (let i = 0; i < matrix.length; i++) {
+			for (let j=0; j<matrix[i].length; j++){
+				let colInd=i%2===0
+				? j
+				:matrix[i].length-j-1
+				ar.push(matrix[i][colInd])
+			}
+		}
+	}
+
+	return ar
 }
